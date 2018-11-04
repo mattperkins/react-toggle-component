544a164094c5ac7d156275b0564592d2138217d0
mattperkins <github@mattperkins.com>
2018-11-04 22:56:53 +0000

    init

---
+++ src/components/Img.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+const style = {
+  width: 300,
+  height: 300,
+  display: "block"
+};
+
+export default () => (
+  <img
+    style={style}
+    alt="image"
+    src="https://images.unsplash.com/photo-1528410390886-e62811f41bed?ixlib=rb-0.3.5&s=5a81344222dab2d34960292c2fa49ba6&auto=format&fit=crop&w=1534&q=80"
+  />
+);
